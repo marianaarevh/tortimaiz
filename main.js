ad5d66e34f0531d06573fb1ca5abd113cd01bef9
Mariana <marianaarevalo933@gmail.com>
2026-03-13 13:28:32 -0700

    primer commit

---
+++ main.js
@@ -0,0 +1,32 @@
+// Cargar navbar
+fetch("components/nav.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("navbar").innerHTML = data;
+
+    let currentPage = window.location.pathname.split("/").pop();
+
+    if(currentPage === ""){
+      currentPage = "index.html";
+    }
+
+    const links = document.querySelectorAll(".nav-link");
+
+    links.forEach(link => {
+      const linkPage = link.getAttribute("href");
+
+      if(linkPage === currentPage){
+        link.classList.add("active");
+      } else {
+        link.classList.remove("active");
+      }
+    });
+
+  });
+
+// Cargar footer
+fetch("components/footer.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("footer").innerHTML = data;
+  });
